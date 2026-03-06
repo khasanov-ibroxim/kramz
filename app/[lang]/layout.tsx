07946766f97d10cf313c92ae9114cbd6dd5af62a
khasanov_ibroxim <xibroxim11@gmail.com>
2026-03-07 00:20:39 +0500

    first

---
+++ app/[lang]/layout.tsx
@@ -0,0 +1,30 @@
+
+import { i18n, Locale } from "@/i18n-config";
+import { getCommonDictionary } from "@/lib/dictionary";
+import { notFound } from "next/navigation";
+
+export async function generateStaticParams() {
+    return i18n.locales.map((locale) => ({ lang: locale }));
+}
+
+export default async function LangLayout({
+                                             children,
+                                             params
+                                         }: {
+    children: React.ReactNode,
+    params: Promise<{ lang: Locale }>
+}) {
+    const { lang } = await params;
+
+    if (!i18n.locales.includes(lang)) {
+        notFound();
+    }
+
+    const dict = await getCommonDictionary(lang);
+
+    return (
+        <>
+            {children}
+        </>
+    );
+}
